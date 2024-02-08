b8a405e48e4c64e5c5479e4f57caee5ea2c00438
VeNtoVkA133 <svyatoslav.stolyarov.s.sss2000@mail.ru>
2024-02-08 13:26:02 +0300

    foolproof
    Добавлена защита от дурака 189 - 225 строки

--- DiskretAlgebraLogic/Form1.Designer.cs
+++ DiskretAlgebraLogic/Form1.Designer.cs
@@ -69,6 +69,9 @@
             this.listBox33 = new System.Windows.Forms.ListBox();
             this.listBox34 = new System.Windows.Forms.ListBox();
             this.listBox35 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // listBox1
@@ -252,6 +255,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(19, 20);
             this.textBox1.TabIndex = 19;
+            this.textBox1.Text = "0";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
@@ -260,6 +264,7 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(19, 20);
             this.textBox2.TabIndex = 20;
+            this.textBox2.Text = "0";
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // textBox3
@@ -268,6 +273,7 @@
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(19, 20);
             this.textBox3.TabIndex = 21;
+            this.textBox3.Text = "0";
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // textBox4
@@ -276,11 +282,12 @@
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(19, 20);
             this.textBox4.TabIndex = 22;
+            this.textBox4.Text = "0";
             this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(60, 144);
+            this.button2.Location = new System.Drawing.Point(59, 179);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(98, 23);
             this.button2.TabIndex = 23;
@@ -442,11 +449,43 @@
             this.listBox35.Size = new System.Drawing.Size(33, 17);
             this.listBox35.TabIndex = 25;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 26);
+            this.label1.TabIndex = 41;
+            this.label1.Text = "Введите значения ( 0 либо 1 )\r\nСтандартное значение = 0\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(39, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 42;
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(39, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 43;
+            this.label3.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(957, 660);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.listBox20);
             this.Controls.Add(this.listBox21);
             this.Controls.Add(this.listBox22);
@@ -538,6 +577,9 @@
         private System.Windows.Forms.ListBox listBox33;
         private System.Windows.Forms.ListBox listBox34;
         private System.Windows.Forms.ListBox listBox35;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
